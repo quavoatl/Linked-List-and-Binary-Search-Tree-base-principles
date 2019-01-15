38ba08e00bbc337f7fa5555028c3436e8de372bd
Antal Sergiu-Alexandru <45335304+quavoatl@users.noreply.github.com>
2019-01-15 23:12:56 +0200

    Add files via upload
    I tried to implement some of the base principles of a Linked List and Binary Search Tree functions, like addItem, removeItem, traverseList.
    Binary Search Tree is still to be finished.

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+
+
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
+
+        String[] data = stringData.split(" ");
+        for(String valueFromData: data ) {
+            list.addItem(new Node(valueFromData));
+        }
+
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("9"));
+        list.removeItem(new Node("0"));
+        list.traverse(list.getRoot());
+
+    }
+}
---
+++ SearchTree.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class SearchTree implements NodeList {
+
+    private ListItem root = null;
+
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            this.root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(newItem);
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(newItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.setPrevious(newItem);
+                } else {
+                    currentItem.setPrevious(newItem);
+
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        if (root != null) {
+            traverse(root.previous());
+            System.out.println(root.getValue());
+            traverse(root.next());
+        }
+    }
+}
